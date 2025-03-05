fb1ae31a2285b50a4b7afe0a296eaaa4796a8b24
Aatif Ali <aatifali8808@gmail.com>
2025-03-05 22:11:29 +0530

    Create DeterminetheScore.cpp

---
+++ DeterminetheScore.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int test, x, n, c, d;
+	std::cin >> test;
+	while (test--){
+	    std::cin >> x >> n;
+	    c=x/10;
+	    d=c*n;
+	    std::cout << d << std::endl;
+	}
+
+}
