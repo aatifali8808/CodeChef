f6968392b99440f3209e32ed6fbbe9843eba8369
Aatif Ali <aatifali8808@gmail.com>
2025-03-05 21:01:55 +0530

    Update HowManyUnattemptedProblems.cpp

---
+++ HowManyUnattemptedProblems.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int total, attempted, un_attempted;
+	std::cin >> total >> attempted;
+    un_attempted = total - attempted;
+    cout << un_attempted << endl;
+    return 0;
+}
